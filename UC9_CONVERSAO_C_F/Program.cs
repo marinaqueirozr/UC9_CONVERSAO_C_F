5af112a0e83f48c89f17f7ef540ed15aadb3e5b6
marinaqueirozr <marinaqueiroz1506@gmail.com>
2023-03-07 20:09:47 -0300

    Finalizado

--- UC9_CONVERSAO_C_F/Program.cs
+++ UC9_CONVERSAO_C_F/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double temperatura = 0;
+double conversao = 0;
+string opcao = "C";
+
+Console.WriteLine("Qual o valor da TEMPERATURA que deseja converter?");
+temperatura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Deseja converter para qual UNIDADE DE TEMPERATURA?");
+Console.WriteLine("C - Graus CELSIUS");
+Console.WriteLine("F - Graus FAHRENHEIT");
+opcao = Console.ReadLine();
+
+Console.WriteLine("");
+
+switch (opcao)
+{
+    case "C":
+        // (0 °C x 9/5) + 32 = 32 °F
+        conversao = (temperatura * 9 / 5) + 32;
+        break;
+
+    case "F":
+        // (0 °F -32) x 5/9 = -17,78 °C
+        conversao = (temperatura - 32) * 9 / 5;
+        break;
+
+    default:
+
+        break;
+}
+
+Console.WriteLine("Temperatura convertida é: " + conversao);
